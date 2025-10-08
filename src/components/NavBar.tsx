1ab9ff8aa1d94eb83f828d8b964dbf8c5ef8e20f
SamGombs20 <gombajosh18@gmail.com>
2025-10-08 14:56:58 +0300

    Update:Table set-up

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import "./NavBar.css"
 const NavBar =()=>{
     return (
         <div className="navbar-container">
-            <p className="navbar-title">Course Tracker</p>
+            <p className="navbar-title">Course<span className="navbar-title">Tracker</span></p>
             <button className="nav-btn">Sign out</button>
         </div>
     );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import "./MainPage.css"
 const MainPage =()=>{
     return (
         <div className="main-page-container">
+            <p className="greeting-text">Hello <span className="name">John</span></p>
             <p className="table-title">Your courses</p>
             <CourseTable/>
         </div>
