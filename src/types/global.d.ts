a2106e1aea2f77641c9abc945861cb05b4fccfef
SamGombs20 <gombajosh18@gmail.com>
2025-10-08 10:37:10 +0300

    Update:Initial set-up

---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+type Course ={
+    id:string;
+    name:string;
+    category:string;
+    description:string;
+    status:"Not Started" | "In Progress" | "Completed";
+    startDate?:string;
+    endDate?:string;
+    rating?:number;
+}
