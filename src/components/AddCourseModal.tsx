3fee9e2567fef293323403b0a22cdc018d659b1a
SamGombs20 <gombajosh18@gmail.com>
2025-10-09 14:35:05 +0300

    Update:Course form modal

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -2,9 +2,10 @@ import TextField from "@mui/material/TextField"
 import { modalStyle, textFieldStyles } from "../styles/MUICustom"
 import { useState, type FC } from "react";
 import { nanoid } from "nanoid";
-import { Box, Modal } from "@mui/material";
+import { Box, FormControl, InputLabel, MenuItem, Modal, Select } from "@mui/material";
 
-const AddCourseModal:FC<AddCourseProps> = ({open, handleClose}) => {
+
+const AddCourseModal: FC<AddCourseProps> = ({ open, handleClose }) => {
     const [course, setCourse] = useState<Course>({
         id: '',
         name: '',
@@ -37,7 +38,16 @@ const AddCourseModal:FC<AddCourseProps> = ({open, handleClose}) => {
                         <TextField label="Course name" name="name" value={course.name} sx={textFieldStyles} onChange={handleChange} />
                         <TextField label="Category" name="category" sx={textFieldStyles} value={course.category} onChange={handleChange} />
                         <TextField label="Description" name="description" sx={textFieldStyles} multiline rows={3} onChange={handleChange} />
-                        <TextField label="Status" name="status" value={course.status} sx={textFieldStyles} onChange={handleChange} />
+                        
+                        <FormControl sx={textFieldStyles}>
+                            <InputLabel id="course-status">Status</InputLabel>
+                            <Select labelId="course-status" id="status" label="Status" name="status"
+                                 onChange={handleChange}>
+                                <MenuItem value="Not Started">Not Started</MenuItem>
+                                <MenuItem value="In Progress">In Progress</MenuItem>
+                                <MenuItem value="Completed">Completed</MenuItem>
+                            </Select>
+                        </FormControl>
                         <TextField label="Start Date" name="startDate" value={course.startDate} onChange={handleChange} sx={textFieldStyles} type="date" slotProps={{
                             inputLabel: { shrink: true }
                         }} />
