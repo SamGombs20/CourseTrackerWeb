67fcdd260595b3f98ab34e143c9abf181bd338f0
SamGombs20 <gombajosh18@gmail.com>
2025-10-09 09:44:40 +0300

    Update:Course table design

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,15 @@
+import { MdAddBox } from "react-icons/md";
 import CourseTable from "../components/CourseTable";
 import "./MainPage.css"
 const MainPage =()=>{
     return (
         <div className="main-page-container">
             <p className="greeting-text">Hello, <span className="name">John</span></p>
-            <p className="table-title">Your courses</p>
-            <CourseTable/>
+            <div className="table-header">
+                <p className="table-title">Your courses</p>
+                <MdAddBox className="add-btn"/>
+            </div>
+            <CourseTable/> 
         </div>
     );
 }
