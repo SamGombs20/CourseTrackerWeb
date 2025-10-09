0d5b0a1a5ce8baf200011664665e0675506a99f9
SamGombs20 <gombajosh18@gmail.com>
2025-10-09 11:10:29 +0300

    Update:Course form design

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,8 +3,8 @@ import CourseTable from "../components/CourseTable";
 import "./MainPage.css"
 import { TextField } from "@mui/material";
 import { textFieldStyles } from "../styles/MUICustom";
-const MainPage =()=>{
-    const onAddCourse =()=>{
+const MainPage = () => {
+    const onAddCourse = () => {
         console.log("Add course clicked");
     }
     return (
@@ -12,14 +12,26 @@ const MainPage =()=>{
             <p className="greeting-text">Hello, <span className="name">John</span></p>
             <div className="table-header">
                 <p className="table-title">Your courses</p>
-                <MdAddBox className="add-btn" onClick={onAddCourse}/>
+                <MdAddBox className="add-btn" onClick={onAddCourse} />
             </div>
-            <CourseTable/> 
+            <CourseTable />
             <div className="course-form-container">
                 <p className="title-text form-title">Add Course</p>
                 <form action="" className="course-form">
-                    <TextField label="Course name" sx={textFieldStyles}/>
-                    <TextField label="Category" sx={textFieldStyles}/>
+                    <TextField label="Course name" sx={textFieldStyles} />
+                    <TextField label="Category" sx={textFieldStyles} />
+                    <TextField label="Description" sx={textFieldStyles} multiline rows={3} />
+                    <TextField label="Status" sx={textFieldStyles} />
+                    <TextField label="Start Date" sx={textFieldStyles} type="date" slotProps={{
+                        inputLabel: { shrink: true }
+                    }} />
+                    <TextField label="End Date" sx={textFieldStyles} type="date" slotProps={{
+                        inputLabel: { shrink: true }
+                    }} />
+                    <div className="form-buttons">
+                        <button>Cancel</button>
+                        <button>Add</button>
+                    </div>
                 </form>
             </div>
         </div>
