dcc510f1491678f6c0c4556eec488d738f811d21
SamGombs20 <gombajosh18@gmail.com>
2025-10-15 12:56:55 +0300

    Update:Course details modal

--- src/components/CourseDetailsModal.tsx
+++ src/components/CourseDetailsModal.tsx
@@ -1,9 +1,31 @@
+import "./CourseDetailsModal.css";
 const CourseDetailsModal =()=>{
     return (
         <div>
-            <p className="title-text">Course title</p>
-            <p>Course Category</p>
-            <p>Course Description</p>
+            <div className="detail-container">
+                <p className="title-text title">Course Name</p>
+                <p>React for beginners</p>
+            </div>
+            <div className="detail-container">
+                <p className="title-text title">Course Category</p>
+                <p>Web Development</p>
+            </div>
+            <div className="detail-container">
+                <p className="title-text title">Course Description</p>
+                <p>Learn the basics of React.js and build dynamic web applications.</p>
+            </div>
+            <div className="detail-container">
+                <p className="title-text title">Status</p>
+                <p>In Progress</p>
+            </div>
+            <div className="detail-container">
+                <p className="title-text title">Duration</p>
+                <p>10/23/2024 - 12/13/2025</p>
+            </div>
+            <div className="detail-container">
+                <p className="title-text title">Rating</p>
+                <p>4.7</p>
+            </div>
         </div>
     );
 }
